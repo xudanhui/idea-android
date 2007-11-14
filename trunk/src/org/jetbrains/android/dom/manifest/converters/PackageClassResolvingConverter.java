41a4808560582bd1003af69c9f5ddaa6b37b934a
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2007-11-14 11:43:12 +0000

    validation and completion for activity class names

--- trunk/src/org/jetbrains/android/dom/manifest/converters/PackageClassResolvingConverter.java
+++ trunk/src/org/jetbrains/android/dom/manifest/converters/PackageClassResolvingConverter.java
@@ -1,19 +1,21 @@
 package org.jetbrains.android.dom.manifest.converters;
 
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.xml.ConvertContext;
-import com.intellij.util.xml.DomElement;
-import com.intellij.util.xml.ResolvingConverter;
-import com.intellij.util.xml.GenericDomValue;
+import com.intellij.psi.search.searches.ClassInheritorsSearch;
+import com.intellij.util.Processor;
+import com.intellij.util.Query;
+import com.intellij.util.xml.*;
 import org.jetbrains.android.dom.manifest.Manifest;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 
 /**
  * @author yole
@@ -21,7 +23,30 @@ import java.util.Collections;
 public class PackageClassResolvingConverter extends ResolvingConverter<PsiClass> {
     @NotNull
     public Collection<? extends PsiClass> getVariants(ConvertContext context) {
-        return Collections.emptyList();
+        final List<PsiClass> result = new ArrayList<PsiClass>();
+        DomElement domElement = context.getInvocationElement();
+        Manifest manifest = domElement.getParentOfType(Manifest.class, true);
+        final String packageName = manifest == null ? null : manifest.getPackage().getValue();
+        ExtendClass extendClass = domElement.getAnnotation(ExtendClass.class);
+        if (extendClass != null && packageName != null) {
+            PsiClass baseClass = context.findClass(extendClass.value(),
+                    GlobalSearchScope.allScope(context.getPsiManager().getProject()));
+            if (baseClass != null) {
+                Query<PsiClass> query = ClassInheritorsSearch.search(baseClass,
+                        GlobalSearchScope.moduleWithDependenciesScope(context.getModule()),
+                        true);
+                query.forEach(new Processor<PsiClass>() {
+                    public boolean process(PsiClass psiClass) {
+                        PsiDirectory directory = psiClass.getContainingFile().getContainingDirectory();
+                        if (directory.getPackage().getQualifiedName().equals(packageName)) {
+                            result.add(psiClass);
+                        }
+                        return true;
+                    }
+                });
+            }
+        }
+        return result;
     }
 
     public PsiClass fromString(@Nullable @NonNls String s, ConvertContext context) {
