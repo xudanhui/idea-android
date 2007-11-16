c35332964b3d114d267e44bef87654b29b54dab2
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2007-11-16 20:40:52 +0000

    add support creates AndroidManifest

--- src/org/jetbrains/android/facet/AndroidSupportConfigurable.java
+++ src/org/jetbrains/android/facet/AndroidSupportConfigurable.java
@@ -3,22 +3,34 @@ package org.jetbrains.android.facet;
 import com.intellij.facet.FacetManager;
 import com.intellij.facet.ModifiableFacetModel;
 import com.intellij.facet.impl.FacetUtil;
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.ide.fileTemplates.FileTemplateUtil;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.ide.util.newProjectWizard.FrameworkSupportConfigurable;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.startup.StartupManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Properties;
 
 /**
  * @author yole
  */
 public class AndroidSupportConfigurable extends FrameworkSupportConfigurable {
+    private static final Logger LOG = Logger.getInstance("#org.jetbrains.android.facet.AndroidSupportConfigurable");
+
     private TextFieldWithBrowseButton mySdkPathField;
     private JPanel myPanel;
     private boolean myInitialized = false;
@@ -40,6 +52,29 @@ public class AndroidSupportConfigurable extends FrameworkSupportConfigurable {
         facet.getConfiguration().SDK_PATH = mySdkPathField.getText();
         model.addFacet(facet);
         model.commit();
+
+        final VirtualFile[] files = modifiableRootModel.getContentRoots();
+        if (files.length > 0) {
+            final Project project = module.getProject();
+            StartupManager.getInstance(project).registerPostStartupActivity(new Runnable() {
+                public void run() {
+                    createAndroidManifest(project, files[0]);
+                }
+            });
+        }
+    }
+
+    private void createAndroidManifest(Project project, VirtualFile file) {
+        PsiDirectory directory = PsiManager.getInstance(project).findDirectory(file);
+        if (directory != null) {
+            FileTemplate template = FileTemplateManager.getInstance().getJ2eeTemplate("AndroidManifest.xml");
+            Properties properties = FileTemplateManager.getInstance().getDefaultProperties();
+            try {
+                FileTemplateUtil.createFromTemplate(template, "AndroidManifest.xml", properties, directory);
+            } catch (Exception e) {
+                LOG.error(e);
+            }
+        }
     }
 
     private void createUIComponents() {
