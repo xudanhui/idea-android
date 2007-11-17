8d3a7bcbed5e0e7a1176487d94123f6b7e763816
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2007-11-17 13:58:23 +0000

    parse filenames and line numbers in compiler error messages

--- src/org/jetbrains/android/compiler/AndroidCompileUtil.java
+++ src/org/jetbrains/android/compiler/AndroidCompileUtil.java
@@ -3,18 +3,33 @@ package org.jetbrains.android.compiler;
 import com.intellij.openapi.compiler.CompileContext;
 import com.intellij.openapi.compiler.CompilerMessageCategory;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author yole
  */
 public class AndroidCompileUtil {
+    private static Pattern ourMessagePattern = Pattern.compile("(.+):(\\d+):.+");
+
     static void addMessages(CompileContext context, Map<CompilerMessageCategory, List<String>> messages) {
         for (CompilerMessageCategory category : messages.keySet()) {
             List<String> messageList = messages.get(category);
             for (String message : messageList) {
-                context.addMessage(category, message, null, -1, -1);
+                String url = null;
+                int line = -1;
+                Matcher matcher = ourMessagePattern.matcher(message);
+                if (matcher.matches()) {
+                    String fileName = matcher.group(1);
+                    if (new File(fileName).exists()) {
+                        url = "file://" + fileName;
+                        line = Integer.parseInt(matcher.group(2));
+                    }
+                }
+                context.addMessage(category, message, url, line, -1);
             }
         }
     }
