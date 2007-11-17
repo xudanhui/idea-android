2cab116423a352f74af152870f80ccfa88b79b7e
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2007-11-17 15:16:44 +0000

    beginning of DOM for resource files

---
+++ src/org/jetbrains/android/dom/resources/Resources.java
@@ -0,0 +1,14 @@
+package org.jetbrains.android.dom.resources;
+
+import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.SubTagList;
+
+import java.util.List;
+
+/**
+ * @author yole
+ */
+public interface Resources extends DomElement {
+    @SubTagList("string")
+    List<ResourceString> getStrings();
+}
