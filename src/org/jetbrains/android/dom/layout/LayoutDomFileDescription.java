19857d3e051302606ab071d799b3a90dd39d5588
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2007-11-28 16:23:25 +0000

    stub of DOM for <layout> (doesn't work yet)

---
+++ src/org/jetbrains/android/dom/layout/LayoutDomFileDescription.java
@@ -0,0 +1,39 @@
+package org.jetbrains.android.dom.layout;
+
+import com.intellij.util.xml.DomFileDescription;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.android.facet.AndroidFacet;
+
+/**
+ * @author yole
+ */
+public class LayoutDomFileDescription extends DomFileDescription<LayoutElement> {
+    public LayoutDomFileDescription() {
+        super(LayoutElement.class, "LinearLayout");
+    }
+
+    public boolean acceptsOtherRootTagNames() {
+        return true;
+    }
+
+    public boolean isMyFile(@NotNull XmlFile file, @Nullable Module module) {
+        if (module == null) return false;
+        VirtualFile virtualFile = file.getVirtualFile();
+        if (virtualFile == null) return false;
+        VirtualFile parent = virtualFile.getParent();
+        if (parent == null || !parent.getName().equals("layout")) {
+            return false;
+        }
+        AndroidFacet facet = AndroidFacet.getInstance(module);
+        if (facet == null) {
+            return false;
+        }
+        VirtualFile p = parent.getParent();
+        VirtualFile resourcesDir = facet.getResourcesDir();
+        return p == resourcesDir;
+    }
+}
---
+++ src/org/jetbrains/android/dom/layout/LayoutElement.java
@@ -0,0 +1,10 @@
+package org.jetbrains.android.dom.layout;
+
+import org.jetbrains.android.dom.AndroidDomElement;
+
+/**
+ * @author yole
+ */
+public interface LayoutElement extends AndroidDomElement {
+    String getId();
+}
