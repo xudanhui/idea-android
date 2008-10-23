fc96fca7b482c2322ebf70723b496dca55057d24
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2008-10-23 10:23:36 +0000

    [1.0] android:name instead of class

--- trunk/src/org/jetbrains/android/dom/manifest/Provider.java
+++ trunk/src/org/jetbrains/android/dom/manifest/Provider.java
@@ -1,16 +1,17 @@
 package org.jetbrains.android.dom.manifest;
 
-import com.intellij.util.xml.*;
 import com.intellij.psi.PsiClass;
+import com.intellij.util.xml.*;
+import org.jetbrains.android.dom.AndroidAttributeValue;
 import org.jetbrains.android.dom.converters.PackageClassResolvingConverter;
 
 /**
  * @author yole
  */
 public interface Provider extends DomElement {
-    @Attribute("class")
+    @Attribute("name")
     @Required
     @Convert(PackageClassResolvingConverter.class)
     @ExtendClass("android.content.ContentProvider")
-    GenericAttributeValue<PsiClass> getProviderClass();
+    AndroidAttributeValue<PsiClass> getProviderClass();
 }
