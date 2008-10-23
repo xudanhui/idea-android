961af7744d6274133458d68772c1129d5fb36a5d
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2008-10-23 10:24:23 +0000

    [diana] don't rely on notnull Module to detect resource file

--- trunk/src/org/jetbrains/android/dom/resources/ResourcesDomFileDescription.java
+++ trunk/src/org/jetbrains/android/dom/resources/ResourcesDomFileDescription.java
@@ -1,12 +1,9 @@
 package org.jetbrains.android.dom.resources;
 
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.util.Comparing;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.util.xml.DomFileDescription;
-import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,13 +17,13 @@ public class ResourcesDomFileDescription extends DomFileDescription<Resources> {
 
     public boolean isMyFile(@NotNull XmlFile file, @Nullable Module module) {
         PsiDirectory psiDirectory = file.getContainingDirectory();
-        if (psiDirectory != null && module != null) {
-            AndroidFacet facet = AndroidFacet.getInstance(module);
-            if (facet == null) return false;
-            VirtualFile directory = psiDirectory.getVirtualFile();
-            return directory.getName().equals("values") &&
-                    Comparing.equal(directory.getParent(), facet.getResourcesDir());
+        if (psiDirectory == null || !"values".equals(psiDirectory.getName())) {
+            return false;
         }
-        return false;
+        psiDirectory = psiDirectory.getParent();
+        if (psiDirectory == null || !"res".equals(psiDirectory.getName())) {
+            return false;
+        }
+        return true;
     }
 }
