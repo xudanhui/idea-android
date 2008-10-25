635a97327864db980f00fe1be60676e774d1c680
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2008-10-25 10:07:27 +0000

    [1.0] class -> android:name

--- src/org/jetbrains/android/dom/manifest/Instrumentation.java
+++ src/org/jetbrains/android/dom/manifest/Instrumentation.java
@@ -1,16 +1,17 @@
 package org.jetbrains.android.dom.manifest;
 
-import com.intellij.util.xml.*;
 import com.intellij.psi.PsiClass;
+import com.intellij.util.xml.*;
+import org.jetbrains.android.dom.AndroidAttributeValue;
 import org.jetbrains.android.dom.converters.PackageClassResolvingConverter;
 
 /**
  * @author yole
  */
 public interface Instrumentation extends DomElement {
-    @Attribute("class")
+    @Attribute("name")
     @Required
     @Convert(PackageClassResolvingConverter.class)
     @ExtendClass("android.app.Instrumentation")
-    GenericAttributeValue<PsiClass> getInstrumentationClass();
+    AndroidAttributeValue<PsiClass> getInstrumentationClass();
 }
