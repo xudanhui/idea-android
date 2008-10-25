32ddf1bf06de92cfaa966388162aaccc6234da01
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2008-10-25 10:46:07 +0000

    [diana] don't use module for detecting layout DOM files

--- trunk/src/org/jetbrains/android/dom/resources/ResourcesDomFileDescription.java
+++ trunk/src/org/jetbrains/android/dom/resources/ResourcesDomFileDescription.java
@@ -16,11 +16,20 @@ public class ResourcesDomFileDescription extends DomFileDescription<Resources> {
     }
 
     public boolean isMyFile(@NotNull XmlFile file, @Nullable Module module) {
+        return isInResourceDirectory(file, "values");
+    }
+
+    public static boolean isInResourceDirectory(XmlFile file, String dirName) {
         PsiDirectory psiDirectory = file.getContainingDirectory();
-        if (psiDirectory == null || !"values".equals(psiDirectory.getName())) {
+        if (psiDirectory == null || !dirName.equals(psiDirectory.getName())) {
             return false;
         }
         psiDirectory = psiDirectory.getParent();
-        return psiDirectory != null && ("res".equals(psiDirectory.getName()) || "default".equals(psiDirectory.getName()));
+        if (psiDirectory == null) return false;
+        if ("default".equals(psiDirectory.getName())) {
+            psiDirectory = psiDirectory.getParentDirectory();
+        }
+
+        return psiDirectory != null && "res".equals(psiDirectory.getName());
     }
 }
