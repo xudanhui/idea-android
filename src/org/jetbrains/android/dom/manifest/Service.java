8d49400759fb8ee45c8de2d5640b414bf12e1621
intelliyole <intelliyole@db6d6eb8-cd3e-0410-97d0-afb2eff4f138>
2008-10-25 10:01:57 +0000

    [1.0] class -> android:name

--- src/org/jetbrains/android/dom/manifest/Service.java
+++ src/org/jetbrains/android/dom/manifest/Service.java
@@ -1,16 +1,17 @@
 package org.jetbrains.android.dom.manifest;
 
-import com.intellij.util.xml.*;
 import com.intellij.psi.PsiClass;
+import com.intellij.util.xml.*;
+import org.jetbrains.android.dom.AndroidAttributeValue;
 import org.jetbrains.android.dom.converters.PackageClassResolvingConverter;
 
 /**
  * @author yole
  */
 public interface Service extends DomElement {
-    @Attribute("class")
+    @Attribute("name")
     @Required
     @Convert(PackageClassResolvingConverter.class)
     @ExtendClass("android.app.Service")
-    GenericAttributeValue<PsiClass> getServiceClass();
+    AndroidAttributeValue<PsiClass> getServiceClass();
 }
